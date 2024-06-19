93c5c1fc25b7640f210b514046f82e7e1559a82b
Zitschaey <105106054+Zitschaey@users.noreply.github.com>
2024-06-19 11:50:06 +0200

    V.1.2.1 -> Funktionierende Kommunikation

--- OPC_UA/src/main/Main.java
+++ OPC_UA/src/main/Main.java
@@ -1,15 +1,15 @@
 package main;
 
 import multithreading.ParallelTaskExecutor;
-import services.DatabaseService;
 import system_instances.EtsDidacticSystem;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		ParallelTaskExecutor parallelTaskExecutor = new ParallelTaskExecutor(new EtsDidacticSystem());
 //		parallelTaskExecutor.executeParallelTasks();
-		parallelTaskExecutor.executeAlarmCheck();
 //		DatabaseService.insertAlarms();
+		parallelTaskExecutor.executeAlarmCheck();
+
 
 	}
 
